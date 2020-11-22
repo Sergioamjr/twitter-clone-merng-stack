d3861a2864d81951ca4368d79c2a368deec539a5
Sérgio Júnior <sergioamjr91@gmail.com>
2020-11-22 17:24:43 -0300

    Setting up eslint

---
+++ web/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <p>Hello Next!</p>
+    </div>
+  );
+}
