641877dad314295035d263145861f38d456e992c
Sérgio Júnior <sergioamjr91@gmail.com>
2020-11-22 20:59:30 -0300

    Added blue color on theme

--- web/components/button/styled.ts
+++ web/components/button/styled.ts
@@ -1,17 +1,18 @@
 import { colors } from "./../../theme/index";
 import styled, { css } from "styled-components";
+import { darken } from "polished";
 
 const ButtonStyle = css`
   border: 0;
   border-radius: 50px;
   padding: 5px 15px;
   cursor: pointer;
-  background-color: ${colors.darkBackground};
+  background-color: ${colors.blue};
   color: ${colors.white};
   font-size: 1rem;
   transition: background-color 0.3s ease;
   &:hover {
-    background-color: #0e171f;
+    background-color: ${darken(0.2, colors.blue)};
   }
 `;
 
--- web/theme/index.ts
+++ web/theme/index.ts
@@ -2,4 +2,5 @@ export const colors = {
   white: "#fff",
   darkBackground: "#15202b",
   light: "rgb(56, 68, 77)",
+  blue: "#39a2f2",
 };
