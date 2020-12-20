7eac313b0db51f63219630d3241613f81c684740
Sérgio Júnior <sergioamjr91@gmail.com>
2020-12-20 13:11:20 -0300

    Doing login

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from "react";
 import {
   useGetTweetsQuery,
+  useLoginMutation,
   useNewTweetMutation,
 } from "../src/generated/graphql";
 import Home from "./../src/features/home";
+import { LoggedUser } from "./../src/generated/graphql";
+
+const userAuthentication = {
+  email: "sergio@gmail.com",
+  password: "S3nh4!@#",
+};
 
 export default function HomePage(): JSX.Element {
   const { data, refetch } = useGetTweetsQuery();
   const [fn] = useNewTweetMutation();
+  const [login] = useLoginMutation();
+  const [user, setUser] = useState<Partial<LoggedUser>>({});
+
+  useEffect(() => {
+    (async () => {
+      const auth = await login({
+        variables: userAuthentication,
+      });
+      const {
+        data: {
+          login: { userName, name, email, token, _id },
+        },
+      } = auth;
+      setUser({ userName, name, email, token, _id });
+    })();
+  }, []);
 
   const onSubmitNewTweet = async (content) => {
     await fn({
       variables: {
-        token:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmRhZjQ0NjRlNzM0MzA5MDg5ZTY0M2YiLCJpYXQiOjE2MDgxODQ5MDJ9.SyQydJtNIzuNWpTdzmAoFLx5Bo4SL04sAsGdxLHwS90",
+        token: user.token,
         content,
       },
     });
