463b5f76ce3bbda895c4016bcb3e1334d4396810
Sérgio Júnior <sergioamjr91@gmail.com>
2021-01-01 12:47:13 -0300

    Fixing alias path config on storybook

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,4 +1,21 @@
+const path = require("path");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   stories: ["../components/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        "~components": path.resolve(__dirname, "src", "components"),
+        "~features": path.resolve(__dirname, "src", "features"),
+        "~graphql": path.resolve(__dirname, "src", "graphql"),
+        "~icons": path.resolve(__dirname, "src", "icons"),
+        "~theme": path.resolve(__dirname, "src", "theme"),
+        "~store": path.resolve(__dirname, "src", "store"),
+      },
+    };
+    return config;
+  },
 };
