06e17b254b1eb6fcca905777a697f314aadd2dab
Sérgio Júnior <sergioamjr91@gmail.com>
2021-01-04 20:55:55 -0300

    Added mdules.d.ts

---
+++ web/modules.d.ts
@@ -0,0 +1,7 @@
+declare module "~components";
+declare module "~features";
+declare module "~graphql";
+declare module "~generated";
+declare module "~icons";
+declare module "~theme";
+declare module "~store";
