45f20dc58a99ad338033b7f9a139157733b392bf
Sérgio Júnior <sergioamjr91@gmail.com>
2021-01-17 20:02:50 -0300

    Setting up server with express

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,39 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import expressPlayground from "graphql-playground-middleware-express";
 import typeDefs from "./src/typeDefs";
 import resolvers from "./src/resolvers";
 import { dbConnect, models } from "./src/database";
+
+const port = process.env.PORT || 4000;
+const app = express();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: (): any => models,
+  introspection: true,
+  playground: true,
+});
+
+server.applyMiddleware({ app });
+
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+app.get("/starter", (req, res) => {
+  res.send("Hello World!");
 });
 
 dbConnect
   .then(() => {
     console.log("DB Connected");
-    server.listen().then(({ url }: { url: string }) => {
-      console.log(`🚀  Server ready at ${url}`);
+    app.listen({ port }, () => {
+      console.log(
+        `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
+      );
     });
   })
   .catch((err) => {
