326e6f68b7370e5719ef37bca0b2c1746d780b05
Sérgio Júnior <sergioamjr91@gmail.com>
2021-01-24 21:36:38 -0300

    Creating user on get into the site

--- web/src/features/auth/index.tsx
+++ web/src/features/auth/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, ReactNode, useState } from "react";
-import { useLoginMutation } from "~graphql/generated/graphql";
+import { useSaveUserMutation } from "~graphql/generated/graphql";
 import { Loading } from "~icons";
 import { actions } from "~store";
 import * as Styled from "./styles";
@@ -8,39 +8,46 @@ type AuthProps = {
   children: ReactNode;
 };
 
+const getNewUser = async () => {
+  const response = await fetch(
+    "https://randomuser.me/api/?inc=name,email,login,picture?nat=BR"
+  );
+  const { results } = await response.json();
+  return {
+    name: `${results[0].name.first} ${results[0].name.last}`,
+    email: results[0].email,
+    password: results[0].login.password,
+    userName: results[0].login.username,
+  };
+};
+
+const KEY = "tww";
 export default function Auth({ children }: AuthProps): JSX.Element {
   const [isLoading, setIsLoading] = useState(true);
-  const [login] = useLoginMutation();
+  const [onSaveUser] = useSaveUserMutation();
+
+  const fetchFakeUser = async () => {
+    const user = await getNewUser();
+    const {
+      data: { saveUser },
+    } = await onSaveUser({
+      variables: user,
+    });
+    window.localStorage.setItem(KEY, JSON.stringify(saveUser));
+    actions.setUserNameAction(saveUser);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    (async () => {
-      try {
-        const auth = await login({
-          variables: {
-            email: process.env.NEXT_PUBLIC_EMAIL,
-            password: process.env.NEXT_PUBLIC_PASSWORD,
-          },
-        });
-        const {
-          data: {
-            login: { userName, name, email, token, _id, friends },
-          },
-        } = auth;
-
-        actions.setUserNameAction({
-          userName,
-          name,
-          email,
-          token,
-          _id,
-          friends,
-        });
-        setIsLoading(false);
-      } catch (err) {
-        console.log("err", err);
-      }
-    })();
+    const user = localStorage.getItem(KEY);
+    if (!user) {
+      fetchFakeUser();
+    } else {
+      actions.setUserNameAction(JSON.parse(user));
+      setIsLoading(false);
+    }
   }, []);
+
   if (isLoading) {
     return (
       <Styled.LoadingWrapper>
