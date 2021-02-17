8460abb297a5063d86dfe0bcc8f7c890230feb2e
Sérgio Júnior <sergioamjr91@gmail.com>
2021-02-17 11:18:41 -0300

    Added GA tracking id

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -4,13 +4,17 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import store from "~store";
 import { GlobalStyle } from "~theme";
 
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+const isProduction = process.env.NODE_ENV === "production";
+
 const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_SERVER_URL,
+  uri: SERVER_URL,
   cache: new InMemoryCache(),
 });
 
 type Props = {
-  Component: any;
+  Component: JSX.Element;
   pageProps: any;
 };
 
@@ -20,6 +24,26 @@ export default function MyApp({ Component, pageProps }: Props): JSX.Element {
       <Head>
         <title>Explore / Twitter</title>
         <link rel="icon" href="/favicon.ico" />
+        {isProduction && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+          `,
+              }}
+            />
+          </>
+        )}
       </Head>
       <GlobalStyle />
       <Provider store={store}>
